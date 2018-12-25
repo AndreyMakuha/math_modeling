f9db2ca7fd78f20b8cebe01ab212e09e30a82601
Andrey Makuha <amakuha1912@gmail.com>
2018-12-25 09:13:18 +0300

    Some cleanup.

--- lab4/src/main.rs
+++ lab4/src/main.rs
@@ -15,30 +15,35 @@ fn main() {
         }
 
         let clust_data = hit_chances(&nums);
+        let el = emperical_law(&nums);
 
         let coords = coordinates_of_intervals(&nums);
         println!("Теоретические частоты попадания случайной величины в интервалы");
-        // for i in 0..coords.len() - 1 {
-        //     println!("от {:.2} - до {:.2}: {:.2} {:.2}", coords[i], coords[i + 1], clust_data[i], el[i]);
-        // }
-            let mut s = String::new();
+        let mut s = String::new();
 
-    for i in 0..coords.len() - 1 {
-        s += &format!("| {:.3} - {:.3} ", coords[i], coords[i + 1]);
-    }
+        for i in 0..coords.len() - 1 {
+            s += &format!("| {:.3} - {:.3} ", coords[i], coords[i + 1]);
+        }
 
-    println!("+{}+", "-".repeat(s.len()-1));
+        println!("+{}+", "-".repeat(s.len()-1));
 
-    for i in 0..coords.len() - 1 {
-        print!("| {:.3} - {:.3} ", coords[i], coords[i + 1]);
-    }
-    println!("|");
-    println!("+{}+", "-".repeat(s.len()-1));
-    for i in 0..coords.len() - 1 {
-        print!("|{:^14.3} ", clust_data[i]);
-    }
-    println!("|");
-    println!("+{}+", "-".repeat(s.len()-1));
+        for i in 0..coords.len() - 1 {
+            print!("| {:.3} - {:.3} ", coords[i], coords[i + 1]);
+        }
+        println!("|");
+        println!("+{}+", "-".repeat(s.len()-1));
+        for i in 0..coords.len() - 1 {
+            print!("|{:^14.3} ", clust_data[i]);
+        }
+
+        println!("|");
+        println!("+{}+", "-".repeat(s.len()-1));
+        for i in 0..coords.len() - 1 {
+            print!("|{:^14.3} ", el[i].1);
+        }
+
+        println!("|");
+        println!("+{}+", "-".repeat(s.len()-1));
     }
     println!("{}", hypotesis_check(&nums, significance_level_answer));
 }
